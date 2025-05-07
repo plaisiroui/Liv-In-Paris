52f4d071c2023b5b0647044f86e056f1ae4cb558
Mateo <mateodubreuil5@gmail.com>
2025-05-07 11:32:15 +0200

    Add files via upload

---
+++ Livrable2/ClientSerializable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Text.Json;
+
+
+namespace Livrable2
+{
+    public class ClientSerializable
+    {
+        public string Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Adresse { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        public string TypeClient { get; set; }
+    }
+}
